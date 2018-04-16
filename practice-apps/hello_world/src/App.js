25701b1a64b07cbc5ac5dc6f63519112162344ae
jeffrothwell <rothwelljeff@gmail.com>
2018-04-16 17:55:30 -0400

    remove sample code and replace with hello_world example

--- practice-apps/hello_world/src/App.js
+++ practice-apps/hello_world/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// bring in React and Component from React
 
-class App extends Component {
-  render() {
+import React, {Component} from 'react';
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render?
+  render () {
+
+    // make sure to return some UI
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Waddup Chompers</h1>
-        </header>
-        <p className="App-intro">
-          dis my app now
-        </p>
-      </div>
-    );
+      <h1>Hello World!</h1>
+    )
   }
 }
 
-export default App;
+export default Hello
