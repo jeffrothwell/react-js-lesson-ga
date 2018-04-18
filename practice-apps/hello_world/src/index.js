cf759cb5ae8bfa0c88b5ce28fa06686e0643e2b6
jeffrothwell <rothwelljeff@gmail.com>
2018-04-18 13:56:46 -0400

    use object to define props outside render function

--- practice-apps/hello_world/src/index.js
+++ practice-apps/hello_world/src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import Hello from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+var person = {
+  personName: "Jeff",
+  personAge: 34
+};
+
 ReactDOM.render(
-  <Hello name={"Nick"} age={24} />,
+  <Hello
+    name={person.personName}
+    age={person.personAge}
+  />,
   document.getElementById('root')
 );
 registerServiceWorker();
