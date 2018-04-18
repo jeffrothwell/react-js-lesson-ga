3526a4e570e392e45175929f88b03fa69289b949
jeffrothwell <rothwelljeff@gmail.com>
2018-04-18 15:12:59 -0400

    complete blog post

--- practice-apps/blog_post/src/App.js
+++ practice-apps/blog_post/src/App.js
@@ -5,14 +5,11 @@ import './App.css';
 class Post extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>{this.props.title}</h1>
+        <h3>by: {this.props.author}</h3>
+        <p>{this.props.body}</p>
+        <p>Comments: {this.props.comments[1]}</p>
       </div>
     );
   }
