636cfa0a7e6bc01be83c8ca9e9d51b41d5b0428e
jeffrothwell <rothwelljeff@gmail.com>
2018-04-19 12:13:22 -0400

    fill in JSX for Movie return block and import into App.js

--- practice-apps/lord-of-the-rings/src/Movie.js
+++ practice-apps/lord-of-the-rings/src/Movie.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class Movie extends Component {
   render() {
     return (
-      // Add JSX here
+      <div>
+        <h1>The Lord of The Rings: A trilogy</h1>
+        <p>Runtime: </p>
+      </div>
     )
   }
 }
+
+export default Movie
