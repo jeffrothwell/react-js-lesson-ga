fad1ab38c16877c062a551b3fc63292483b08bcf
jeffrothwell <rothwelljeff@gmail.com>
2018-04-19 10:58:45 -0400

    render all the comments

--- practice-apps/blog_post/src/App.js
+++ practice-apps/blog_post/src/App.js
@@ -6,13 +6,20 @@ import Comment from './Comment.js';
 
 class Post extends Component {
   render() {
+
+    let allComments = [
+      <Comment body={this.props.comments[0]} />,
+      <Comment body={this.props.comments[1]} />,
+      <Comment body={this.props.comments[2]} />
+    ]
+
     return (
       <div>
         <h1>{this.props.title}</h1>
         <h3>by: {this.props.author}</h3>
         <p>{this.props.body}</p>
         <p>Comments:</p>
-        <Comment body={this.props.comments[0]} />
+        {allComments}
       </div>
     );
   }
