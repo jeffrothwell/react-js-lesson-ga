a0e4ede40845fddbe55e0c7b4df3d5647613b953
jeffrothwell <rothwelljeff@gmail.com>
2018-04-23 13:10:16 -0400

    add constructor to define and set initial state for moodPoints

--- practice-apps/blog_post/src/Comment.js
+++ practice-apps/blog_post/src/Comment.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 
 class Comment extends Component {
+  constructor (props) {
+    // super() makes sure we first load all funcitonality of the Component class
+    super()
+    this.state = { moodPoints: 1 }
+  }
+
   render () {
     return (
       <div>
