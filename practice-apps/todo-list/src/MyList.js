0bd2851873348bf4efbb9f848661aca29745c1d8
jeffrothwell <rothwelljeff@gmail.com>
2018-04-24 13:35:48 -0400

    rename file to MyList, populate w basic list and reflect changes in index.js

---
+++ practice-apps/todo-list/src/MyList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class MyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This stuff needs to get done:</h1>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default MyList;
