61ab84a87b276987e216bcda0949553dfab9efc4
jeffrothwell <rothwelljeff@gmail.com>
2018-04-24 13:37:03 -0400

    formatting and get rid of serviceWorker stuff

--- practice-apps/todo-list/src/index.js
+++ practice-apps/todo-list/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import MyList from './MyList';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MyList />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <MyList />,
+  document.getElementById('root')
+);
