8f283016b029de775144ed49027ef8635199b51f
jeffrothwell <rothwelljeff@gmail.com>
2018-04-25 10:52:07 -0400

    add form so new item can be added to the list

--- practice-apps/todo-list/src/MyList.js
+++ practice-apps/todo-list/src/MyList.js
@@ -5,13 +5,34 @@ class MyList extends Component {
   constructor(props) {
     super()
 
-    this.state = { stateListItems: props.listItems }
+    this.state = {
+      stateListItems: props.listItems,
+      newItem: ""
+    }
   }
 
   clearList (e) {
     console.log("Clearing the list!");
     this.setState({
       stateListItems: []
+    })
+  }
+
+  newItemChange (e) {
+    console.log("Changing newItem");
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem (e) {
+    console.log("Adding newItem to list");
+    e.preventDefault()
+    var currentList = this.state.stateListItems
+    currentList.push(this.state.newItem)
+    this.setState({
+      newItem: "",
+      stateListItems: currentList,
     })
   }
 
@@ -27,6 +48,15 @@ class MyList extends Component {
         <ol>
           {listItemRenders}
         </ol>
+        <form>
+          <input type="text"
+           placeholder="Type an item here"
+           onChange={(e) => this.newItemChange(e)}
+           value={this.state.newItem}
+          />
+          <button onClick={(e) => this.addItem(e)}>Add it!</button>
+        </form>
+
         <button onClick={(e) => this.clearList(e)}>Finished The List</button>
       </div>
     );
