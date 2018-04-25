fc161d81869eabde7463371d6579028d7a9db100
jeffrothwell <rothwelljeff@gmail.com>
2018-04-24 22:37:43 -0400

    store listItems prop in initial state and use this in .map render array cration

--- practice-apps/todo-list/src/MyList.js
+++ practice-apps/todo-list/src/MyList.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import ListItem from './ListItem';
 
 class MyList extends Component {
+  constructor(props) {
+    super()
+
+    this.state = { stateListItems: props.listItems }
+  }
+
   render() {
 
-    let listItemRenders = this.props.listItems.map( (item, index) => {
+    let listItemRenders = this.state.stateListItems.map( (item, index) => {
       return <ListItem doThis={item} key={index} />
     })
 
     return (
       <div class="list-container">
         <h1>This stuff needs to get done:</h1>
-        <ul>
+        <ol>
           {listItemRenders}
-        </ul>
+        </ol>
       </div>
     );
   }
