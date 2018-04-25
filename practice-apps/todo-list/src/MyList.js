adcd5fb59ff1583163e45aa992d1ac7bf667278a
jeffrothwell <rothwelljeff@gmail.com>
2018-04-24 20:41:14 -0400

    minimal weak-sauce styling

--- practice-apps/todo-list/src/MyList.js
+++ practice-apps/todo-list/src/MyList.js
@@ -4,7 +4,7 @@ import ListItem from './ListItem';
 class MyList extends Component {
   render() {
     return (
-      <div>
+      <div class="list-container">
         <h1>This stuff needs to get done:</h1>
         <ul>
           <ListItem />
