de2f45525a7719977d6d58a5daa4f12d20a0563b
jeffrothwell <rothwelljeff@gmail.com>
2018-05-08 15:05:14 -0400

    create mt Calculator component

---
+++ practice-apps/calculator/src/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="Calculator">
+        
+      </div>
+    );
+  }
+}
+
+export default Calculator;
