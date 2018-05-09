64f3b2a259ab3bbef8a225bbe9e692f66f40ac94
jeffrothwell <rothwelljeff@gmail.com>
2018-05-08 20:39:05 -0400

    insert basic calculator structure

--- practice-apps/calculator/src/Calculator.js
+++ practice-apps/calculator/src/Calculator.js
@@ -6,7 +6,15 @@ class Calculator extends Component {
   render() {
     return (
       <div className="Calculator">
-        
+        <h1>Add with React!</h1>
+
+        <div className="add">
+           <input type="text" />
+           <span>+</span>
+           <input type="text" />
+           <span>=</span>
+           <h3>Addition results go here!</h3>
+        </div>
       </div>
     );
   }
