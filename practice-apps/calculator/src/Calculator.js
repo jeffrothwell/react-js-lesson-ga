df20eb23388695792b03b58911d8937c83c8192f
jeffrothwell <rothwelljeff@gmail.com>
2018-05-25 10:34:52 -0400

    simplify state to sum, use refs to get values from fields

--- practice-apps/calculator/src/Calculator.js
+++ practice-apps/calculator/src/Calculator.js
@@ -5,40 +5,18 @@ class Calculator extends Component {
   constructor(props) {
     super()
 
-    this.state = {
-      num1: 0,
-      num2: 0,
-      sum: 0
-    }
+    this.state = { sum: 0 }
 
-    this.updateNum1 = this.updateNum1.bind(this)
-    this.updateNum2 = this.updateNum2.bind(this)
     this.calculateSum = this.calculateSum.bind(this)
   }
 
   calculateSum() {
     console.log(this.state);
-    var newSum = (this.state.num1) + (this.state.num2);
+    var newSum = (parseInt(this.refs.val1.value) || 0) + (parseInt(this.refs.val2.value) || 0);
     console.log(newSum);
     this.setState({
       sum: newSum
     })
-  }
-
-  updateNum1(e) {
-    var newNum1 = parseInt(e.target.value)
-    this.setState({
-      num1: newNum1
-    })
-    this.calculateSum()
-  }
-
-  updateNum2(e) {
-    var newNum2 = parseInt(e.target.value)
-    this.setState({
-      num2: newNum2
-    })
-    this.calculateSum()
   }
 
   render() {
@@ -49,14 +27,14 @@ class Calculator extends Component {
         <div className="add">
            <input
               type="text"
-              onChange={(e) => this.updateNum1(e)}
-              value={this.state.num1}
+              ref="val1"
+              onKeyUp={this.calculateSum}
             />
            <span>+</span>
            <input
              type="text"
-             onChange={(e) => this.updateNum2(e)}
-             value={this.state.num2}
+             ref="val2"
+             onKeyUp={this.calculateSum}
             />
            <span>=</span>
            <h3>{this.state.sum}</h3>
