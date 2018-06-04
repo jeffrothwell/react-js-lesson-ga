3bb012d0225e2c2df0e55eb3706e74ffc42c81a0
jeffrothwell <rothwelljeff@gmail.com>
2018-06-04 19:45:27 -0400

    switch to functional component

--- practice-apps/todo-list/src/ListItem.js
+++ practice-apps/todo-list/src/ListItem.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
-class ListItem extends Component {
-  render(){
-    return (
-      <li>{this.props.doThis}</li>
-    )
-  }
-}
+const ListItem = props => (
+  <li>{this.props.doThis}</li>
+)
 
 export default ListItem;
