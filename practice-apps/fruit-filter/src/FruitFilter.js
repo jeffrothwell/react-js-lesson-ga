f9641bcba6675f03e46a23bb5f49811f4d27a69a
jeffrothwell <rothwelljeff@gmail.com>
2018-06-05 08:46:23 -0400

    rename App to FruitFilter and clear out UI contents

---
+++ practice-apps/fruit-filter/src/FruitFilter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FruitFilter extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default FruitFilter;
--- practice-apps/fruit-filter/src/index.js
+++ practice-apps/fruit-filter/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FruitFilter />, document.getElementById('root'));
 registerServiceWorker();
