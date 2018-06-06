c55a4aeaf4aadd0b31e65d99efb80f7920e65996
jeffrothwell <rothwelljeff@gmail.com>
2018-06-06 12:57:36 -0400

    write function to handle filter changes

--- practice-apps/fruit-filter/src/FruitFilter.js
+++ practice-apps/fruit-filter/src/FruitFilter.js
@@ -4,9 +4,25 @@ class FruitFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fruitsToDisplay = props.fullFruitList,
-      filterValue = '';
+      fruitsToDisplay: props.fullFruitList,
+      filterValue: '';
     }
+  }
+
+  handleFilterChange(e) {
+    e.preventDefault()
+
+    const filterValue = e.target.value
+    this.setState((prevState, props) => {
+      // Filter down the list of fruits
+      const filteredFruitList = props.fullFruitList.filter(fruit =>
+      fruit.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase()))
+
+      return {
+        fruitsToDisplay: filteredFruitList,
+        filterValue
+      }
+    })
   }
 
   render() {
