a2548c4ad11a247c78e7df8839af66d84772abb9
jeffrothwell <rothwelljeff@gmail.com>
2018-06-06 17:40:56 -0400

    remove default content, add basic stuff

--- practice-apps/dentist-site/src/App.js
+++ practice-apps/dentist-site/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h1>Dentist Site</h1>
+        <p>
+          Welcome to my dentist site.
         </p>
       </div>
     );
