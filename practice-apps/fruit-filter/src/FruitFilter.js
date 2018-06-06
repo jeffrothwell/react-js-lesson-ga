cee4e7e8a25c69f7df85780f47cfa0956992f496
jeffrothwell <rothwelljeff@gmail.com>
2018-06-06 12:33:36 -0400

    set up constructor

--- practice-apps/fruit-filter/src/FruitFilter.js
+++ practice-apps/fruit-filter/src/FruitFilter.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class FruitFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fruitsToDisplay = props.fullFruitList,
+      filterValue = '';
+    }
+  }
+
   render() {
     return (
       <div>
