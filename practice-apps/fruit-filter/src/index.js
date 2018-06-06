46825a7160dece578dd710bec22ba2113bf86edd
jeffrothwell <rothwelljeff@gmail.com>
2018-06-06 12:33:18 -0400

    import FruitFilter and call render on it

--- practice-apps/fruit-filter/src/index.js
+++ practice-apps/fruit-filter/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FruitFilter from './FruitFilter';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<FruitFilter />, document.getElementById('root'));
