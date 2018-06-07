c8c9c8db1b8afdc1cd02481bac592c9186d583b8
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 10:19:01 -0400

    move home stuff to component, call within App

--- practice-apps/dentist-site/src/App.js
+++ practice-apps/dentist-site/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Dentist Site</h1>
-        <p>
-          Welcome to my dentist site.
-        </p>
+        <Home></Home>
       </div>
     );
   }
---
+++ practice-apps/dentist-site/src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dentist Site</h1>
+        <p>
+          Welcome to my dentist site.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
