3aaf0df65d2007bf4f99d16bb4b69598e6fa9971
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 16:55:04 -0400

    convert procedures to array of objects

--- practice-apps/dentist-site/src/index.js
+++ practice-apps/dentist-site/src/index.js
@@ -4,28 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const procedures = {
-  spitShine: {
+const procedures = [
+  {
     name: "Spit Shine",
     cost: 50,
     timeInMinutes: 30
   },
-  deepClean: {
+  {
     name: "Deep Clean",
     cost: 200,
     timeInMinutes: 60
   },
-  cavityFill: {
+  {
     name: "Cavity Filling",
     cost: 400,
     timeInMinutes: 60
   },
-  rootCanal: {
+  {
     name: "Root Canal",
     cost: 600,
     timeInMinutes: 120
-  },
-}
+  }
+]
 
 ReactDOM.render(<App procedures={procedures} />,
   document.getElementById('root')
