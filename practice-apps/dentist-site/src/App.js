4fb1a8b1bab2fc20bffe5d9d0cb76e82d04cd56c
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 12:55:49 -0400

    implement router

--- practice-apps/dentist-site/src/App.js
+++ practice-apps/dentist-site/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Home from './Home';
 import Procedures from './Procedures';
 import Contact from './Contact';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Dentist Site</h1>
-        <Home></Home>
-        <Procedures></Procedures>
-        <Contact></Contact>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/procedures" component={Procedures} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </Router>
       </div>
     );
   }
