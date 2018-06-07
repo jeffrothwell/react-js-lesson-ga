16d2b36d3350a57135ecdd4d9eeb9117291caf99
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 12:02:46 -0400

    set up a procedures object to pass in

--- practice-apps/dentist-site/src/index.js
+++ practice-apps/dentist-site/src/index.js
@@ -4,5 +4,28 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const procedures = {
+  spitShine: {
+    name: "Spit Shine",
+    cost: 50,
+    timeInMinutes: 30
+  },
+  deepClean: {
+    name: "Deep Clean",
+    cost: 200,
+    timeInMinutes: 60
+  },
+  cavityFill: {
+    name: "Cavity Filling",
+    cost: 400,
+    timeInMinutes: 60
+  },
+  rootCanal: {
+    name: "Root Canal",
+    cost: 600,
+    timeInMinutes: 120
+  },
+}
+
+ReactDOM.render(<App procedures={procedures} />, document.getElementById('root'));
 registerServiceWorker();
