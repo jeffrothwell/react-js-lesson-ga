e8aad19f3a656143fbf0c62fa7618d8cc17e3382
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 16:35:32 -0400

    refactor Post as seperate component, render from App

--- practice-apps/blog_post/src/App.js
+++ practice-apps/blog_post/src/App.js
@@ -1,52 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Post from './Post'
 
 // bring in Comment class
 import Comment from './Comment.js';
 import Author from './Author.js';
 
-class Post extends Component {
-  constructor (props) {
-    super()
-
-    this.state = { body: props.body }
-  }
-
-  updateBody(e){
-    let newBody = prompt("What should the new body be?")
-    this.setState({
-      body: newBody
-    })
-  }
-
-  handleFormInput(e){
-    this.setState({
-      body: e.target.value
-    })
-  }
-
+class App extends Component {
   render() {
-
-    let allAuthors = this.props.authors.map(author => {
-      return <Author body={author} />
-    })
-
-    let allComments = this.props.comments.map(comment => {
-      return <Comment body={comment} />
-    })
-
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        {allAuthors}
-        <p>{this.state.body}</p>
-        <button onClick={(e) => this.updateBody(e)}>Edit Body</button>
-        <input type="text" onChange={(e) => this.handleFormInput(e)} />
-        <p>Comments:</p>
-        {allComments}
+        <
+          Post
+          title={this.props.title}
+          authors={this.props.authors}
+          body={this.props.body}
+          comments={this.props.comments}
+        />
       </div>
     );
   }
 }
 
-export default Post;
+export default App;
