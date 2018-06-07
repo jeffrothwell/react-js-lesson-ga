912f60e0b672de372f839b2fc91d37cf18d7ac4e
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 11:51:34 -0400

    create two other pages based on Home template

---
+++ practice-apps/dentist-site/src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dentist Site</h1>
+        <p>
+          Welcome to my dentist site.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ practice-apps/dentist-site/src/Procedures.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Procedures extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dentist Site</h1>
+        <p>
+          Welcome to my dentist site.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Procedures;
