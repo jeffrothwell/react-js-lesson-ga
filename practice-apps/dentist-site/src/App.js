a9460959fa2c551b52aed549ecd79dcc957ae0d7
jeffrothwell <rothwelljeff@gmail.com>
2018-06-07 12:03:16 -0400

    import Contact and Procedure components

--- practice-apps/dentist-site/src/App.js
+++ practice-apps/dentist-site/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import Procedures from './Procedures';
+import Contact from './Contact';
 import './App.css';
 
 class App extends Component {
