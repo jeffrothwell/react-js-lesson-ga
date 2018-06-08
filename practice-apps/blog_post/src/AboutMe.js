717ea8aa063fcb74e059f03e1d6350b41970256b
jeffrothwell <rothwelljeff@gmail.com>
2018-06-08 11:45:19 -0400

    create functional components for each page

---
+++ practice-apps/blog_post/src/AboutMe.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+const AboutMe = props => (
+  <div>
+    <h3>{props.title}</h3>
+    <p>{props.body}</p>
+  </div>
+)
+
+export default AboutMe
---
+++ practice-apps/blog_post/src/FavouriteFood.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+const FavouriteFood = props => (
+  <div>
+    <h3>{props.title}</h3>
+    <p>{props.body}</p>
+  </div>
+)
+
+export default FavouriteFood
