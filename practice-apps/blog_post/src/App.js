cfb4e1cb6be6b2c974f8e93fb9983c4012045a93
jeffrothwell <rothwelljeff@gmail.com>
2018-06-08 12:50:01 -0400

    add Home component, add router to App

--- practice-apps/blog_post/src/App.js
+++ practice-apps/blog_post/src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Post from './Post'
+import Home from './Home'
 import AboutMe from './AboutMe'
+import TheBestShow from './TheBestShow'
+import FavouriteFood from './FavouriteFood'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Router>
+          <div>
+            <nav>
+              <Link to="/">Home</Link>
+              <Link to="/aboutme">See Our Procedures</Link>
+              <Link to="/contact">Contact Us!</Link>
+            </nav>
+            <Route exact path="/" component={Home} />
+            <Route path="/aboutme" component={
+              () => (<AboutMe aboutMe={this.props.aboutMe}/>)
+            }/>
+            <Route path="/contact" component={Contact} />
+          </div>
+        </Router>
         <h1>Thanks for visiting Wookersons page</h1>
         < Post blogPost={this.props.blogPost} />
         < AboutMe aboutMe={this.props.aboutMe} />
---
+++ practice-apps/blog_post/src/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const FavouriteFood = props => (
+  <div>
+    <h3>Here's a joke I like:</h3>
+    <p>How do cows do math?</p>
+    <p>On a cow-culator.</p>
+  </div>
+)
+
+export default FavouriteFood
