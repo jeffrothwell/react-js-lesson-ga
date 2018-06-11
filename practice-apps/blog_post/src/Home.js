0bcf1d8783766f45af50c5dfb1642035ae2b50d9
jeffrothwell <rothwelljeff@gmail.com>
2018-06-11 16:45:54 -0400

    add random shakespeare poem API call, loading state

--- practice-apps/blog_post/src/Home.js
+++ practice-apps/blog_post/src/Home.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
 
-const FavouriteFood = props => (
-  <div>
-    <h3>Here's a joke I like:</h3>
-    <p>How do cows do math?</p>
-    <p>On a cow-culator.</p>
-    <img src="http://i0.kym-cdn.com/entries/icons/mobile/000/002/149/wook-_17.jpg" alt="Wookerson picture" width="250px"/>
-  </div>
-)
+class Home extends Component {
+  constructor() {
+    super()
 
-export default FavouriteFood
+    this.state = {poem: ""}
+  }
+
+  componentDidMount() {
+    var base = this
+    const url = "http://shakeitspeare.com/api/poem"
+    fetch(url)
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      base.setState({poem: data.poem})
+    }).catch(ex => {
+      console.log("An error occured!", ex);
+    })
+  }
+
+  render () {
+    if (this.state.poem && this.state.poem != "") {
+      return (
+        <div>
+          <h3>Here's a joke I like:</h3>
+          <p>How do cows do math?</p>
+          <p>On a cow-culator.</p>
+          <img src="http://i0.kym-cdn.com/entries/icons/mobile/000/002/149/wook-_17.jpg" alt="Wookerson picture" width="250px"/>
+          <h3>Here's a random Shakespeare poem:</h3>
+          <p>{this.state.poem}</p>
+        </div>
+      )
+    } else {
+        return (
+          <div>
+            <h2>Loading</h2>
+          </div>
+        )
+    }
+  }
+}
+
+
+export default Home
